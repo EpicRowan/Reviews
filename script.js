5df5b5ed64d0f6cc7d019e9046d4f1a0486c18bb
Rowan <shepherd.rowan.k@gmail.com>
2021-04-11 13:26:11 -0700

    Created sample data on js file

---
+++ script.js
@@ -0,0 +1,17 @@
+// sample data
+
+const people = [{
+	id:1,
+	name: "Bob Bobson",
+	review: "5 stars",
+},
+{
+	id:2,
+	name: "Jim Jimmyson",
+	review: "3 stars",
+},
+]
+
+// select items
+
+const
