384bc0be2bda43f4d045fd7ca52105e3ac84a0a3
Rowan <shepherd.rowan.k@gmail.com>
2021-04-11 13:34:49 -0700

    Setting up initial event listeners

--- script.js
+++ script.js
@@ -10,8 +10,20 @@ const people = [{
 	name: "Jim Jimmyson",
 	review: "3 stars",
 },
-]
+];
 
 // select items
 
-const
+const author = document.getElementById("name");
+const review = document.getElementById("review");
+
+const prevBtn = document.querySelector("prev");
+const nextBtn = document.querySelector("next");
+
+let currentItem = 0;
+
+// load first item
+
+window.addEventListener("DOMContentLoaded", function () {
+	console.log("working maybe")
+})
